264f8aa17add5a0baff0ddb4886b34fa28336820
finaldusk <finaldusk@live.cn>
2016-05-20 21:51:48 +0800

    first version

---
+++ getScore.php
@@ -0,0 +1,66 @@
+﻿<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="stylesheet" type="text/css" href="/css/style.css" />
+
+<title>SYNU QueryScore</title>
+</head>
+<body>
+
+
+
+
+
+<?php
+$stuyear=substr($_POST["stuyear"],2,2).$_POST["stumonth"];
+$id=$_POST["id"];
+
+
+
+header("content-type:text/html;charset=utf-8");
+
+//连接Webserver
+$client = new SoapClient("http://210.30.208.140/ahxkp_cjcx_webserver.asmx?wsdl");
+
+//预存参数
+$param = array('Scan1'=>$stuyear,'Scan2'=>$id);
+
+//发送请求
+$p = $client->__soapCall('xscjcx',array('parameters' => $param));
+
+$str= $p->xscjcxResult;
+
+//$str="您的2015-2016学年1学期的考试成绩为 大学外语专1: 73分,数据结构: 84分,职业发展与就业指导上: 合格,大学生安全教育: 合格,大学生心理健康教育: 合格,WINDOWS程序设计: 85分,大学生军事理论教程: 合格,面向对象的程序设计C++: 85分,计算机系统结构: 优秀,科技文献检索与写作: 优秀,算法分析与设计: 优秀,WEB程序设计: 94分.";
+
+//获取标题
+$title_at=strpos($str," ");
+$score_title = substr($str,0,$title_at);
+echo "<h1>";
+echo $score_title;
+echo "</h1></br>";
+
+//去除标题
+$secore = strstr($str," ");
+
+//切割各科分数
+$row=explode(',',$secore);
+$secore_count=count($row);
+?>
+</br>
+<div class=outarea>
+<ul>
+<?php
+//输出
+for ($i=0;$i<$secore_count;$i++){
+	echo "<li>";
+	echo $row[$i];
+	echo "</li>";
+}
+
+?>
+</ul>
+</div>
+
+
+</body>
---
+++ index.php
@@ -0,0 +1,72 @@
+﻿<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="stylesheet" type="text/css" href="/css/style.css" />
+
+<title>SYNU QueryScore</title>
+</head>
+<body>
+<h1>成绩查询</h1>
+</br>
+</br>
+<div class=outarea>
+<div class=inarea >
+
+</br>
+<form action="getScore.php" method="post">
+学号: <input type="text" onkeyup="value=value.replace(/[^\d]/g,'') " onbeforepaste="clipboardData.setData('text',clipboardData.getData('text').replace(/[^\d.]/g,''))" name="id"></br></br>
+学年: <select name="stuyear">
+<?php
+$thisyear=date("Y");
+$thismonth=date("n");
+if ($thismonth>9){
+	$thisyear++;
+}
+for($index=0;$index<4;$index++){
+	
+	$xn=($thisyear-$index-1)."-".($thisyear-$index);
+	echo "<option value=\"".$xn."\">".$xn."</option>";
+}
+
+?>
+</select>
+学期:<select name="stumonth">
+<option value="0">1</option>
+<option value="1">2</option>
+<option value="2">3</option>
+</select>
+</div>
+
+<p text-align=left><input type="submit"></p>
+</form>
+
+</div>
+
+
+
+<script type="text/javascript">
+    (function(win,doc){
+        var s = doc.createElement("script"), h = doc.getElementsByTagName("head")[0];
+        if (!win.alimamatk_show) {
+            s.charset = "gbk";
+            s.async = true;
+            s.src = "http://a.alimama.cn/tkapi.js";
+            h.insertBefore(s, h.firstChild);
+        };
+        var o = {
+            pid: "mm_34249687_3444417_13656743",/*推广单元ID，用于区分不同的推广渠道*/
+            appkey: "",/*通过TOP平台申请的appkey，设置后引导成交会关联appkey*/
+            unid: "",/*自定义统计字段*/
+            type: "click" /* click 组件的入口标志 （使用click组件必设）*/
+        };
+        win.alimamatk_onload = win.alimamatk_onload || [];
+        win.alimamatk_onload.push(o);
+    })(window,document);
+</script>
+
+<div class=outarea>
+<a data-type="3" data-tmpl="250x250" data-tmplid="185" data-rd="2" data-style="2" data-border="1" href="#"></a>
+</div>
+
+</body>

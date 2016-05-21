161d70b72f34c4cd607c770b777fed7e5fa11a96
finaldusk <finaldusk@live.cn>
2016-05-21 08:09:45 +0800

    add style sheet

--- index.php
+++ index.php
@@ -44,29 +44,11 @@ for($index=0;$index<4;$index++){
 </div>
 
 
+</br>
 
-<script type="text/javascript">
-    (function(win,doc){
-        var s = doc.createElement("script"), h = doc.getElementsByTagName("head")[0];
-        if (!win.alimamatk_show) {
-            s.charset = "gbk";
-            s.async = true;
-            s.src = "http://a.alimama.cn/tkapi.js";
-            h.insertBefore(s, h.firstChild);
-        };
-        var o = {
-            pid: "mm_34249687_3444417_13656743",/*推广单元ID，用于区分不同的推广渠道*/
-            appkey: "",/*通过TOP平台申请的appkey，设置后引导成交会关联appkey*/
-            unid: "",/*自定义统计字段*/
-            type: "click" /* click 组件的入口标志 （使用click组件必设）*/
-        };
-        win.alimamatk_onload = win.alimamatk_onload || [];
-        win.alimamatk_onload.push(o);
-    })(window,document);
-</script>
 
 <div class=outarea>
-<a data-type="3" data-tmpl="250x250" data-tmplid="185" data-rd="2" data-style="2" data-border="1" href="#"></a>
+© 2016 <a href="http://www.wangxinlei.cn" title="最後的黄昏">最後的黄昏</a> <a href="http://www.miitbeian.gov.cn/" title="辽ICP备15000815号-2">辽ICP备15000815号-2</a>
 </div>
 
 </body>
